a7177c1ba352a6d974f095f9e2405cfa9daa2315
Ebbe Vang <ev@evang.dk>
2016-02-15 11:30:28 +0100

    adds project

---
+++ Assets/scripts/BeeEnemyLogic.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeEnemyLogic : MonoBehaviour {
+
+    public float AttackDistance;
+    public float Speed;
+    public float SecondsAtAttackPoint;
+
+    private GameObject _player;
+    private Vector3 _pointToAttack;
+    private float _startedWaiting;
+    
+    private enum BeeState { ATTACKING, WAITING, ROAMING };
+
+    private BeeState state;
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        state = BeeState.ROAMING;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // if bee is roaming
+        if (state == BeeState.ROAMING)
+        {
+            if (Vector2.Distance(this.transform.position, _player.transform.position) < AttackDistance)
+            {
+                _pointToAttack = _player.transform.position;
+                state = BeeState.ATTACKING;
+            }
+            Debug.Log("roaming");
+        }
+        //if bee is attacking
+        if (state == BeeState.ATTACKING)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointToAttack, Speed * Time.deltaTime);
+
+            if (this.transform.position.Equals(_pointToAttack))
+            {
+                _startedWaiting = Time.time;
+                state = BeeState.WAITING;
+
+                
+            }
+            Debug.Log("attacking");
+        }
+        //if bee is waiting
+        if (state == BeeState.WAITING)
+        {
+            if (Time.time > _startedWaiting + SecondsAtAttackPoint)
+            {
+                state = BeeState.ROAMING;
+            }
+            
+        }
+
+        
+       
+        
+        
+
+
+	}
+}
---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    public float Speed;
+    public float JumpSpeed;
+    public bool onLadder;
+    public bool onRope;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (onRope || onLadder)
+        {
+            GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpSpeed*100));
+
+        } 
+        if (onRope || onLadder)
+        {
+            transform.position += new Vector3(0, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
+        }
+
+	}
+}
